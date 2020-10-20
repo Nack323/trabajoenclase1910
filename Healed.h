60e8d1bd850f57571f6520b69b79fa1fdde98934
Nack323 <jpalacios117@gmail.com>
2020-10-20 18:47:19 -0500

    Added healed signatures

---
+++ Healed.h
@@ -0,0 +1,13 @@
+#ifndef Healed_h
+#define Healed_h
+class Healed:public Person{
+    private:
+        int numEpochsHealthy;
+        int numEpochsInfected;
+        int numEpochsHealed;
+    public:
+        Healed(int,int,int);
+        void next_move() override;
+        void updateEpoch() override;
+}
+#endif
