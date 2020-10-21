9cbf3e3c1a2612ef5adca230aec47886b71495c3
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 23:02:11 -0600

    Update Virus.cpp

---
+++ Virus.cpp
@@ -0,0 +1,7 @@
+#include "Virus.h"
+#include <iostream> 
+
+Virus::Virus(string name, float probabilityOfInfection, float probabilityAsymptomatic, float probabilityDeath, float probabilityHealed, float probabilityInfected)
+{
+
+}
