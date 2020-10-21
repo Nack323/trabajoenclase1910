348201f221bf22a6c925d4f4a33c20ff0e5b27d9
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 22:43:38 -0600

    Update Healthy.cpp

---
+++ Healthy.cpp
@@ -0,0 +1,22 @@
+#include "Healty.h"
+#include <iostream>
+
+Healty::Healty(int numEpochsHealthy)
+{
+
+}
+
+void Healty::setNumEpochs(int numEpochsHealthy)
+{
+  this->numEpochsHealthy=numEpochsHealthy
+}
+
+void Healty::void next_move()
+{
+  //override
+}
+
+void Healty::void updateEpoch()
+{
+  //override
+}  
