45cd2b08fa2acb0620c06a803e3a6dd8371680ff
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 22:51:43 -0600

    Update Person.cpp

---
+++ Person.cpp
@@ -0,0 +1,17 @@
+#include "Person.h"
+#include <iostream> 
+
+Person::Person(int id)
+{
+
+}
+
+virtual void Person::next_move()
+{
+  //falta
+}
+
+virtual void Person::updateEpoch()
+{
+  //falta
+}
