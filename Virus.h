482576ef683000ac9b69e0051f50ba9637a50ee7
Nack323 <jpalacios117@gmail.com>
2020-10-20 19:13:51 -0500

    Added signatures for virus.h

---
+++ Virus.h
@@ -0,0 +1,15 @@
+#ifndef Virus_h
+#define Virus_h
+
+class Virus{
+    public:
+        std::string name;
+        float probabilityOfInfection;
+        float probabilityAsymptomatic;
+        float probabilityDeath;
+        float probabilityHealed;
+        float probabilityInfected;
+        Virus(std::string, float, float, float, float, float);
+};
+
+#endif
