ed5c237d435d8e314c752885b18f92ee2ee7a427
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 22:34:15 -0600

    Update Death.cpp

---
+++ Death.cpp
@@ -0,0 +1,17 @@
+#include "Death.h"
+#include <iostream>
+
+Death::Death(int numEpochsHealthy, int numEpochsInfected, int epochOfDeath)
+{
+
+}
+
+void Death::next_move()
+{
+  \\override
+}
+
+void Death::updateEpoch()
+{
+  \\override
+}
