503ae4d6ff7ec9f66bf53343e9a6861b7b149761
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 22:46:16 -0600

    Update Hospital.cpp

---
+++ Hospital.cpp
@@ -0,0 +1,12 @@
+#include "Hospital.h"
+#include <iostream>
+
+Hospital::Hospital(int id, int capacity, float increaseProbabilityHealed)
+{
+
+}
+
+virtual void Hospital::acceptPatient()
+{
+  //Falta
+}
