a545bdf2423a8caf560246ae2a9c822bc848c400
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 23:09:55 -0600

    Update World.cpp

---
+++ World.cpp
@@ -0,0 +1,37 @@
+#include "World.h"
+#include <iostream>
+
+World::WorldObject(WorldObject *world, Person *population, Virus virus)
+{
+
+}
+
+void World::newEpoch()
+{
+  //falta
+}
+
+int World::int getInfected()
+{
+  return; //no se que variable es jeje 
+}
+
+int World::getDeath()
+{
+  return; //no se que variable es jeje
+}
+
+int World::getHealty()
+{
+  return; //no se que variable es jeje
+}
+
+int World::getHealed()
+{
+  return; //no se que variable es jeje
+}
+
+void World::printWorld()
+{
+  //falta
+}
