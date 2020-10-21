296af677238e4c4b47eaff626e7f8b6b1ac25322
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 22:37:04 -0600

    Update Healed.cpp

---
+++ Healed.cpp
@@ -0,0 +1,17 @@
+#include "Healed.h"
+#include <iostream>
+
+Healed::Healed(int numEpochsHealthy, int numEpochsInfected, int numEpochsHealed)
+{
+ 
+}
+
+void Healed::next_move()
+{
+  \\override
+}
+
+void Healed::updateEpoch()
+{
+  \\override
+}
