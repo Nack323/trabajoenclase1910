f0d0ec3b2a8d1c8ae87178067a0899411dbc0e42
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-10-20 22:49:10 -0600

    Update Infected.cpp

---
+++ Infected.cpp
@@ -0,0 +1,17 @@
+#include "Infected.h" 
+#include <iostream>
+
+Infected::Infected(int numEpochsHealthy, int numEpochsInfected) 
+{
+
+}
+
+void Infected::void next_move()
+{
+  //override
+}
+
+void Infected::void updateEpoch()
+{
+  //override
+}
